23281d3e9896da75d1723c2a67ce83803ef454d6
msever <mislav.sever@reversinglabs.com>
2024-03-26 17:32:09 +0100

    Add unit tests

--- tests/test_a1000.py
+++ tests/test_a1000.py
@@ -1,4 +1,5 @@
 import pytest
+from ReversingLabs.SDK import __version__
 from ReversingLabs.SDK.a1000 import CLASSIFICATIONS, AVAILABLE_PLATFORMS, A1000
 from ReversingLabs.SDK.helper import WrongInputError
 
@@ -23,13 +24,18 @@ def test_a1000_object():
 	a1000 = A1000(
 		host=valid_host,
 		token="my_mock_token",
-		verify=True,
-		user_agent="my_user_agent"
+		verify=True
 	)
 
 	assert a1000._url == valid_host + "{endpoint}"
 
 	with pytest.raises(WrongInputError, match=r"host parameter must contain a protocol definition at the beginning."):
 		A1000(host=invalid_host, token="my_mock_token")
+
+	with pytest.raises(WrongInputError, match=r"If token is not provided username and password are required."):
+		A1000(host=valid_host)
+
+	user_agent = a1000._headers.get("User-Agent")
+	assert __version__ in user_agent
 
 
