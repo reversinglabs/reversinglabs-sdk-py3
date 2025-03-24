73ed1a486ce0bb4fd8ec067a638de59087180875
msever <mislav.sever@reversinglabs.com>
2025-03-24 17:48:19 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -31,12 +31,10 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Security",
         "Topic :: Software Development :: Libraries :: Python Modules"
