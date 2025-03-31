43de64b7aa9c4d7b46b5279e2f2e044d36845d46
msever <mislav.sever@reversinglabs.com>
2025-03-31 11:01:45 +0200

    Finalize the Spectra Assure flow

--- ReversingLabs/SDK/advanced.py
+++ ReversingLabs/SDK/advanced.py
@@ -1,9 +1,11 @@
 import io
-
 import requests
 
+from typing import Union
+from time import sleep
+
 from ReversingLabs.SDK.helper import DEFAULT_USER_AGENT, WrongInputError
-from ReversingLabs.SDK.ticloud import FileAnalysis, DynamicAnalysis, AdvancedSearch, FileDownload
+from ReversingLabs.SDK.ticloud import FileAnalysis, DynamicAnalysis, FileDownload
 from ReversingLabs.SDK.a1000 import A1000
 
 
@@ -62,57 +64,6 @@ class AdvancedActions(object):
             return rldata_report
 
         return {}
-
-
-class SpectraAssureScenarios(object):
-    """A class for scenarios that include RL Spectra Assure and other RL services from the RLSDK."""
-
-    def __init__(self, spectra_assure_client, verify_certs=True):
-
-        self._spectra_assure_client = spectra_assure_client
-        self.verify_certs = verify_certs
-
-    def a1000_upload_to_assure(self, a1000_host, a1000_token, file_hash, filename, project, package, version):
-        a1000_client = A1000(
-            host=a1000_host,
-            token=a1000_token,
-            user_agent=DEFAULT_USER_AGENT,
-            verify=self.verify_certs
-        )
-
-        file_content = a1000_client.download_sample(sample_hash=file_hash).content
-
-        response = self._spectra_assure_client.submit_package(
-            file=file_content,
-            filename=filename,
-            project=project,
-            package=package,
-            version=version
-        )
-
-        return response
-
-    def ticloud_upload_to_assure(self, ticloud_host, ticloud_username, ticloud_password, file_hash, filename, project,
-                                 package, version):
-        download_client = FileDownload(
-            host=ticloud_host,
-            username=ticloud_username,
-            password=ticloud_password,
-            user_agent=DEFAULT_USER_AGENT,
-            verify=self.verify_certs
-        )
-
-        file_content = download_client.download_sample(hash_input=file_hash).content
-
-        response = self._spectra_assure_client.submit_package(
-            file=file_content,
-            filename=filename,
-            project=project,
-            package=package,
-            version=version
-        )
-
-        return response
 
 
 class SpectraAssureClient(object):
@@ -178,5 +129,196 @@ class SpectraAssureClient(object):
         return response
 
 
+class SpectraAssureScenarios(object):
+    """A class for scenarios that include RL Spectra Assure and other RL services from the RLSDK."""
 
+    def __init__(self, spectra_assure_client: SpectraAssureClient, verify_certs=True):
+        self._spectra_assure_client = spectra_assure_client
+        self.verify_certs = verify_certs
 
+    def __fetch_and_upload_to_assure(self, client: Union[A1000, FileDownload], file_hash, project,
+                                     starting_version, max_version):
+        file_content = client.download_sample(file_hash).content
+        status_code = 409
+
+        starting_version = int(starting_version * 10)
+        max_version = int(max_version * 10)
+
+        current_version = starting_version
+
+        while status_code == 409:
+            if current_version > max_version:
+                break
+
+            response = self._spectra_assure_client.submit_package(
+                file=file_content,
+                filename=file_hash,
+                project=project,
+                package=f"package-{file_hash}",
+                version=float(current_version / 10)
+            )
+
+            status_code = response.status_code
+            current_version += 1
+
+        return float((current_version - 1) / 10)
+
+    def __get_report_from_assure(self, file_hash, report_type, project, current_version, max_retries=6):
+        if max_retries > 30:
+            raise WrongInputError("Maximum number of fetch retries exceeded.")
+
+        not_done = "version that is not done with analysis"
+
+        response = self._spectra_assure_client.get_analysis_report(
+            report_type=report_type,
+            project=project,
+            package=f"package-{file_hash}",
+            version=current_version
+        )
+
+        retry_attempt = 1
+
+        while not_done in response.text and retry_attempt <= max_retries:
+            sleep(3)
+
+            response = self._spectra_assure_client.get_analysis_report(
+                report_type=report_type,
+                project=project,
+                package=f"package-{file_hash}",
+                version=current_version
+            )
+
+            retry_attempt += 1
+
+        return response
+
+    def a1000_upload_to_assure(self, a1000_host, a1000_token, hash_list, project, starting_version=1.0, max_version=12.0,
+                               get_analysis_report=False, report_type=None, max_retries=6) -> dict:
+        """Fetches a list of samples defined in the hash_list from Spectra Analyze and
+        submits them to Spectra Assure for analysis.
+        Since Spectra Assure requires specifying the version of the sample (package),
+        this method tries increasing the version of each sample from the list until it reaches
+        a version that does not exist yet.
+        You can specify the starting version and the maximum version to try with before backing off for
+        the sample that is currently being uploaded.
+            :param a1000_host: host of the desired Spectra Analyze instance, including the protocol prefix
+            :type a1000_host: str
+            :param a1000_token: authorization token of your Spectra Analyze account
+            :type a1000_token: str
+            :param hash_list: list containing the hashes of samples you want to upload to Spectra Assure
+            :type hash_list: list[str]
+            :param project: project name on Spectra Assure
+            :type project: str
+            :param starting_version: the starting version to try with for the current sample that is being uploaded
+            :type starting_version: float
+            :param max_version: the maximum version to try with before backing off for the current sample
+            that is being uploaded
+            :type max_version: float
+            :param get_analysis_report: fetch the analysis report from Spectra Assure
+            :type get_analysis_report: bool
+            :param report_type: type of report to fetch
+            :type report_type: str
+            :param max_retries: maximum number of retries while trying to fetch the report before backing off
+            :type max_retries: int
+            :return: a dict of analysis reports
+            :rtype: dict
+        """
+        if max_version > 12:
+            raise WrongInputError("The max_version can not be higher than 12.")
+
+        a1000_client = A1000(
+            host=a1000_host,
+            token=a1000_token,
+            user_agent=DEFAULT_USER_AGENT,
+            verify=self.verify_certs
+        )
+
+        reports = {}
+
+        for file_hash in hash_list:
+            current_version = self.__fetch_and_upload_to_assure(
+                client=a1000_client,
+                file_hash=file_hash,
+                project=project,
+                starting_version=starting_version,
+                max_version=max_version
+            )
+
+            if get_analysis_report:
+                response = self.__get_report_from_assure(
+                    file_hash=file_hash,
+                    report_type=report_type,
+                    project=project,
+                    current_version=current_version,
+                    max_retries=max_retries
+                )
+
+                reports[file_hash] = response.json()
+
+        return reports
+
+    def ticloud_upload_to_assure(self, ticloud_host, ticloud_username, ticloud_password, hash_list, project,
+                                 starting_version=1, max_version=12, get_analysis_report=False, report_type=None,
+                                 max_retries=6) -> dict:
+        """Fetches a list of samples defined in the hash_list from Spectra Intelligence and
+        submits them to Spectra Assure for analysis.
+        Since Spectra Assure requires specifying the version of the sample (package),
+        this method tries increasing the version of each sample from the list until it reaches
+        a version that does not exist yet.
+        You can specify the starting version and the maximum version to try with before backing off for
+        the sample that is currently being uploaded.
+            :param ticloud_host: Spectra Intelligence host name
+            :type ticloud_host: str
+            :param ticloud_username: your Spectra Intelligence username
+            :type ticloud_username: str
+            :param ticloud_password: your Spectra Intelligence password
+            :type ticloud_password: str
+            :param hash_list: list containing the hashes of samples you want to upload to Spectra Assure
+            :type hash_list: list[str]
+            :param project: project name on Spectra Assure
+            :type project: str
+            :param starting_version: the starting version to try with for the current sample that is being uploaded
+            :type starting_version: float
+            :param max_version: the maximum version to try with before backing off for the current sample
+            that is being uploaded
+            :type max_version: float
+            :param get_analysis_report: fetch the analysis report from Spectra Assure
+            :type get_analysis_report: bool
+            :param report_type: type of report to fetch
+            :type report_type: str
+            :param max_retries: maximum number of retries while trying to fetch the report before backing off
+            :type max_retries: int
+            :return: a dict of analysis reports
+            :rtype: dict
+        """
+        download_client = FileDownload(
+            host=ticloud_host,
+            username=ticloud_username,
+            password=ticloud_password,
+            user_agent=DEFAULT_USER_AGENT,
+            verify=self.verify_certs
+        )
+
+        reports = {}
+
+        for file_hash in hash_list:
+            current_version = self.__fetch_and_upload_to_assure(
+                client=download_client,
+                file_hash=file_hash,
+                project=project,
+                starting_version=starting_version,
+                max_version=max_version
+            )
+
+            if get_analysis_report:
+                response = self.__get_report_from_assure(
+                    file_hash=file_hash,
+                    report_type=report_type,
+                    project=project,
+                    current_version=current_version,
+                    max_retries=max_retries
+                )
+
+                reports[file_hash] = response.json()
+
+        return reports
