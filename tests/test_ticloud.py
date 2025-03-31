43bd532f0b04cd5c5146fc1be240ea4d37c5c09e
msever <mislav.sever@reversinglabs.com>
2025-03-31 13:20:20 +0200

    Create test_advanced.py

--- tests/test_ticloud.py
+++ tests/test_ticloud.py
@@ -10,7 +10,7 @@ from ReversingLabs.SDK.ticloud import TiCloudAPI, FileReputation, AVScanners, Fi
 	VerticalFeedsStatistics, VerticalFeedsSearch, CertificateAnalytics, CertificateThumbprintSearch, \
 	NewMalwarePlatformFiltered, NewFilesFirstScan, NewFilesFirstAndRescan, FilesWithDetectionChanges, \
 	MWPChangeEventsFeed, CvesExploitedInTheWild, NewExploitOrCveSamplesFoundInWildHourly, \
-	NewExploitAndCveSamplesFoundInWildDaily, NewWhitelistedFiles, ChangesWhitelistedFiles, AdvancedActions, \
+	NewExploitAndCveSamplesFoundInWildDaily, NewWhitelistedFiles, ChangesWhitelistedFiles, \
 	CLASSIFICATIONS, AVAILABLE_PLATFORMS, RHA1_TYPE_MAP, \
 	resolve_hash_type, calculate_hash, NotFoundError
 from ReversingLabs.SDK.helper import WrongInputError, BadGatewayError, DEFAULT_USER_AGENT
@@ -1481,61 +1481,3 @@ class TestChangesWhitelistedFiles:
 			self.changes.feed_query(time_format="utc", time_value="12345678")
 
 
-@pytest.fixture
-def dynamic_analysis_mock():
-	with mock.patch("ReversingLabs.SDK.ticloud.DynamicAnalysis.get_dynamic_analysis_results", autospec=True) as dynamic_mock:
-		yield dynamic_mock
-
-
-@pytest.fixture
-def file_analysis_mock():
-	with mock.patch("ReversingLabs.SDK.ticloud.FileAnalysis.get_analysis_results", autospec=True) as rldata_mock:
-		yield rldata_mock
-
-
-class TestAdvancedActions:
-	@classmethod
-	def setup_class(cls):
-		cls.adv_actions = AdvancedActions(HOST, USERNAME, PASSWORD)
-
-	def test_no_da_report(self, dynamic_analysis_mock, file_analysis_mock):
-		dynamic_analysis_mock.return_value.json.return_value = {}
-
-		file_analysis_mock.return_value.json.return_value = {
-			"rl": {
-				"sample": {
-					"sha1": SHA1
-				}
-			}
-		}
-
-		result = self.adv_actions.enriched_file_analysis(sample_hash=SHA1)
-		expected_result = {}
-
-		assert result == expected_result
-
-	def test_existing_da_field(self, dynamic_analysis_mock, file_analysis_mock):
-		dynamic_analysis_mock.return_value.json.return_value = {
-			"rl": {
-				"report": {
-					"da_key": "da_value"
-				}
-			}
-		}
-
-		file_analysis_mock.return_value.json.return_value = {
-			"rl": {
-				"sample": {
-					"sha1": SHA1,
-					"dynamic_analysis": {
-						"entries": [
-							{"existing_field": "existing_value"}
-						]
-					}
-				}
-			}
-		}
-
-		result = self.adv_actions.enriched_file_analysis(sample_hash=SHA1)
-		assert "entries" in result.get("rl").get("sample").get("dynamic_analysis")
-		assert "report" in result.get("rl").get("sample").get("dynamic_analysis")
